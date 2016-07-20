a5ca855b130d600f4b39b862a1924e88db689c08
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-07-20 22:09:54 +0200

    Moved PersistenceConfig.java to dk.surfstation.iscrape.business instead of dk.surfstation.iscrape.business.config ... since it for some reason only works this way!

--- src/main/java/dk/surfstation/iscrape/business/PersistenceConfig.java
+++ src/main/java/dk/surfstation/iscrape/business/PersistenceConfig.java
@@ -1,4 +1,4 @@
-package dk.surfstation.iscrape.business.config;
+package dk.surfstation.iscrape.business;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
